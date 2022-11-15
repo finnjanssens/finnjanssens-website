c02a1da43f50e823aef69944d9e8f8045bdcc3b9
finnjanssens <finnjanssensfj@gmail.com>
2022-11-15 11:30:20 +0100

    Move HomeHero css to component level

---
+++ components/HomeHero/HomeHero.tsx
@@ -0,0 +1,39 @@
+import styles from "./HomeHero.module.css";
+
+export default function HomeHero() {
+  return (
+    <div className="fullscreen__container">
+      <div id="home__hero">
+        <svg
+          viewBox="0 0 300 300"
+          width="300"
+          height="300"
+          className={styles.circleSVG}
+        >
+          <defs>
+            <path
+              id="circle"
+              d="
+        M 0, 140
+        m 10, 10
+        a 140,140 0 1,1 280,0
+        a 140,140 0 1,1 -280,0"
+            />
+          </defs>
+          <text fontSize="17">
+            <textPath xlinkHref="#circle" className={styles.circleTextPath}>
+              Portfolio • Portfolio • Portfolio • Portfolio • Portfolio •
+              Portfolio • Portfolio
+            </textPath>
+          </text>
+        </svg>
+        <h1 className={styles.title}>
+          {/* <span className="light">PORTFOLIO</span> */}
+          FI<span className="stretch">N</span>N
+          <br />
+          JANSS<span className="stretch">E</span>NS
+        </h1>
+      </div>
+    </div>
+  );
+}
