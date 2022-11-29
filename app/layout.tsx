7bf2fa2de9a5a3c57d6b42f1289f490d4fdbb363
finnjanssens <finnjanssensfj@gmail.com>
2022-11-29 10:48:16 +0100

    relocate meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-head-element */
 import Link from "next/link";
 import "./globals.css";
+import Head from "next/head";
 
 export default function RootLayout({
   children,
@@ -9,6 +10,17 @@ export default function RootLayout({
 }) {
   return (
     <html>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+        <meta name="Author" content="Finn Janssens" key="author" />
+        <meta
+          name="description"
+          content="Hi! I’m Finn, a student Multimedia and Creative Technology at Erasmus Colleges Brussels. I’m passionate about creating and love anything technology-related. During my education in Multimedia and Creative Technology I’ve grown a big interest for the front-end, the back-end and everything in between."
+        />
+        <title>Finn Janssens | Portfolio</title>
+      </Head>
       <body>
         <main>
           {/* <nav></nav> */}
