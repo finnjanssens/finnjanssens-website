159cff3010961d265ec3a71c56a328761f39b05e
finnjanssens <finnjanssensfj@gmail.com>
2022-11-29 10:52:36 +0100

    Fix meta data

---
+++ app/head.tsx
@@ -0,0 +1,15 @@
+export default function Head({ params }) {
+  return (
+    <>
+      <meta charSet="utf-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+      <meta name="Author" content="Finn Janssens" key="author" />
+      <meta
+        name="description"
+        content="Hi! I’m Finn, a student Multimedia and Creative Technology at Erasmus Colleges Brussels. I’m passionate about creating and love anything technology-related. During my education in Multimedia and Creative Technology I’ve grown a big interest for the front-end, the back-end and everything in between."
+      />
+      <title>Finn Janssens | Portfolio</title>
+    </>
+  );
+}
